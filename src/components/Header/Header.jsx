4961ec3f516b637bd347683ce40d7b7dc0aeaa5f
mAbdeen9 <93052095+mAbdeen9@users.noreply.github.com>
2022-09-10 17:10:07 +0300

    header css

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { useState } from "react";
+import { Fragment } from "react";
+import classes from "./Header.module.css";
+
+function Header() {
+  const [showMenu, setShowMenu] = useState(false);
+
+  const handelShowMenu = () => setShowMenu((state) => !state);
+
+  return (
+    <Fragment>
+      <div className={`container-fluid ${classes.nav_box}  `}>
+        <div className="container">
+          <div className={classes.nav_bar}>
+            <div>
+              <span className={classes.logo}>
+                Real<i className="bi bi-geo-fill"></i>App
+              </span>
+              <span> About </span>
+            </div>
+            <div className={classes.links}>
+              <span>Sign in</span>
+              <span>Sign up</span>
+              <span>Sign up business</span>
+            </div>
+            <div className={classes.burger}>
+              <div onClick={handelShowMenu} className={classes.boxBurger}>
+                <i className="bi bi-list"> </i>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      {showMenu && (
+        <div className={`container ${classes.mobile_menu}`}>
+          <div>Sign in</div>
+          <div>Sign up</div>
+          <div>Sign up business</div>
+        </div>
+      )}
+    </Fragment>
+  );
+}
+
+export default Header;
