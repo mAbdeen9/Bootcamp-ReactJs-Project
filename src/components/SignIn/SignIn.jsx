bba6f7b0f43bfedf168ec45ff58d887a71ca8ed5
mAbdeen9 <93052095+mAbdeen9@users.noreply.github.com>
2022-09-12 19:40:23 +0300

    sign in

--- src/components/SignIn/SignIn.jsx
+++ src/components/SignIn/SignIn.jsx
@@ -2,27 +2,44 @@ import React from "react";
 import { useState } from "react";
 import { useRef } from "react";
 import useValidate from "../../hooks/useValidate";
-
 import classes from "./Signin.module.css";
+
 function SignIn() {
   const emailValue = useRef();
   const passwordValue = useRef();
   const { validateEmail, validatePassword } = useValidate();
-  const [emailError, setEmailError] = useState(false);
-  const [emailPassword, setEmailPassword] = useState(false);
+  const [emailHasError, setEmailHasError] = useState(false);
+  const [passwordHasError, setPasswordHasError] = useState(false);
+  const [emailIsTouched, setEmailIsTouched] = useState(false);
+  const [passwordIsTouched, setpasswordIsTouched] = useState(false);
 
   const checkEmail = () => {
+    setEmailIsTouched(true);
     const { error } = validateEmail({ email: emailValue.current.value });
-    if (error) setEmailError(true);
-    if (!error) setEmailError(false);
+    if (error) setEmailHasError(true);
+    if (!error) setEmailHasError(false);
   };
 
   const checkPassword = () => {
+    setpasswordIsTouched(true);
     const { error } = validatePassword({
       password: passwordValue.current.value,
     });
-    if (error) setEmailPassword(true);
-    if (!error) setEmailPassword(false);
+    if (error) setPasswordHasError(true);
+    if (!error) setPasswordHasError(false);
+  };
+
+  const validForm =
+    !emailHasError && !passwordHasError && emailIsTouched && passwordIsTouched;
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!validForm) return;
+    const userInfo = {
+      email: emailValue.current.value,
+      password: passwordValue.current.value,
+    };
+    console.log(userInfo);
   };
 
   return (
@@ -30,7 +47,7 @@ function SignIn() {
       <h1>
         Sign in <i className="bi bi-box-arrow-in-right"></i>
       </h1>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className={classes.form_box}>
           <div>
             <label htmlFor="email">
@@ -46,7 +63,7 @@ function SignIn() {
               placeholder="email"
             />
             <br />
-            {emailError && (
+            {emailHasError && (
               <span className={classes.invaild_email}> invalid email !</span>
             )}
           </div>
@@ -65,7 +82,7 @@ function SignIn() {
               maxLength={10}
             />
             <br />
-            {emailPassword && (
+            {passwordHasError && (
               <span className={classes.invaild_password}>
                 invalid password !
               </span>
